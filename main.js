0a0736784a9c301253ab26f0a673f9968cdcf124
matias <matias.gonzalo.lopez@gmail.com>
2020-02-04 22:05:33 -0500

    js and css refactor

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 // variables
-// const navbar = document.querySelector(".home-nav");
 const navList = document.querySelector(".nav-list");
 const navLogo = document.querySelector("nav img");
 const navElements = document.querySelectorAll(".nav-list li");
 const spanElements = document.querySelectorAll(".nav-list span");
 const burger = document.querySelector(".burger");
-const lineElements = document.querySelectorAll(".burger .line");
+const lineElements = document.querySelectorAll(".burger div");
+const largeScreenMQ = window.matchMedia("(min-width: 1024px)");
 
 //sets home-page height to 100vh
 window.addEventListener("resize", () => {
@@ -15,36 +15,15 @@ window.addEventListener("resize", () => {
 
 //click mobile menu
 burger.addEventListener("click", () => {
-  if (!navList.classList.contains("mobile-nav-transition")) {
-    navList.classList.toggle("mobile-nav-transition");
-  }
-  navList.classList.toggle("close");
+  navList.classList.add("opacity-transition");
   navList.classList.toggle("open");
-  if (navList.classList.contains("open")) {
-    // lineElements[0].classList.add("line-transition");
-    lineElements[0].classList.add("line-up-rotation");
-    lineElements[1].classList.add("line-invisible");
-    lineElements[2].classList.add("line-down-rotation");
-
-    spanElements.forEach(spanElement =>
-      spanElement.classList.add("span-pointer")
-    );
-  } else {
-    lineElements[0].classList.remove("line-up-rotation");
-    lineElements[1].classList.remove("line-invisible");
-    lineElements[2].classList.remove("line-down-rotation");
-    // lineElements[0].classList.remove("line-transition");
-    spanElements.forEach(spanElement =>
-      spanElement.classList.remove("span-pointer")
-    );
-  }
+  burger.classList.toggle("cross");
+  spanElements.forEach(span => span.classList.toggle("span-pointer"));
 });
 
-// when linding to the site, if viewport (vp) is bigger than 1024px
-let largeScreenMQ = window.matchMedia("(min-width: 1024px)");
+// when landing to the site, if viewport (vp) is bigger than 1024px
 if (largeScreenMQ.matches) {
   window.addEventListener("scroll", changeNav);
-  navList.classList.remove("close");
   navElements.forEach(function(navElement) {
     if (navElement.innerText !== "INSTAGRAM") {
       navElement.classList.add("expanded");
@@ -68,11 +47,8 @@ largeScreenMQ.addListener(() => {
 function widthChange(largeScreenMQ) {
   if (largeScreenMQ.matches) {
     window.addEventListener("scroll", changeNav);
-    navList.classList.remove("close");
-    navList.classList.remove("open");
     navList.classList.remove("mobile-nav-transition");
     navElements.forEach(function(navElement) {
-      // navElement.classList.remove("transition");
       if (navElement.innerText !== "INSTAGRAM") {
         navElement.classList.add("expanded");
         if (navElement.innerText === "CONTACT") {
@@ -84,21 +60,18 @@ function widthChange(largeScreenMQ) {
     });
   } else {
     window.removeEventListener("scroll", changeNav);
-    navList.classList.add("close");
-    navList.classList.remove("nav-transparent");
-    navList.classList.remove("nav-white");
-    navList.classList.remove("nav-short");
-    navList.classList.remove("desktop-nav-transition");
-    navList.classList.remove("nav-white-transition");
-    navLogo.classList.remove("line-transition");
+    editClasses(
+      navList,
+      ["nav-transparent", "desktop-nav-transition", "nav-white", "nav-short"],
+      false
+    );
     navLogo.classList.remove("invisible");
-    lineElements[0].classList.remove("line-up-rotation");
-    lineElements[1].classList.remove("line-invisible");
-    lineElements[2].classList.remove("line-down-rotation");
     navElements.forEach(function(navElement) {
-      navElement.classList.remove("visible");
-      navElement.classList.remove("invisible");
-      navElement.classList.remove("desktop-nav-transition");
+      editClasses(
+        navElement,
+        ["visible", "invisible", "desktop-nav-transition"],
+        false
+      );
       if (navElement.innerText !== "INSTAGRAM") {
         navElement.classList.remove("expanded");
         if (navElement.innerText === "CONTACT") {
@@ -107,6 +80,7 @@ function widthChange(largeScreenMQ) {
       } else {
         navElement.classList.remove("instagram-expanded");
         navElement.classList.remove("expanded");
+        editClasses(navList, ["expanded", "instagram-expanded"], false);
       }
     });
   }
@@ -116,12 +90,9 @@ function widthChange(largeScreenMQ) {
 function changeNav() {
   let scrolled = window.pageYOffset;
   if (scrolled === 0) {
+    editClasses(navList, ["nav-transparent", "nav-short"], false);
     navList.classList.add("desktop-nav-transition");
-    navList.classList.remove("nav-transparent");
-    navList.classList.remove("nav-white-transition");
-    navList.classList.remove("nav-short");
     navLogo.classList.remove("invisible");
-    // navLogo.classList.remove("line-transition");
     navElements.forEach(function(navElement) {
       if (
         navElement.innerText !== "ABOUT" &&
@@ -135,14 +106,11 @@ function changeNav() {
           navElement.classList.add("instagram-expanded");
           navElement.classList.remove("expanded");
         }
-        // navElement.style.textShadow = "none";
       }
     });
   } else {
     navList.classList.add("nav-transparent");
-    if (!navList.classList.contains("nav-white-transition")) {
-      navList.classList.add("desktop-nav-transition");
-    }
+    navList.classList.add("desktop-nav-transition");
     navElements.forEach(function(navElement) {
       navElement.classList.add("desktop-nav-transition");
       if (
@@ -157,31 +125,16 @@ function changeNav() {
           navElement.classList.remove("instagram-expanded");
           navElement.classList.add("expanded");
         }
-        // navElement.style.textShadow = "1.8px 1.8px 2px white";
       }
     });
   }
-  // console.log("scrolled", scrolled);
   if (scrolled > window.innerHeight) {
-    // console.log(window.innerHeight);
-    navList.classList.remove("nav-transparent");
-    navList.classList.remove("desktop-nav-transition");
-    navList.classList.add("nav-white-transition");
-    navList.classList.add("nav-white");
-    navList.classList.add("nav-short");
-    navLogo.classList.add("line-transition");
-    navLogo.classList.add("invisible");
-    // navList.style.float = "right";
-    // navList.style.right = "0";
-    // navList.style.backgroundColor = "white";
-    // navList.style.width = "350px";
+    editClasses(navList, ["nav-transparent", "desktop-nav-transition"], false);
+    editClasses(navList, ["nav-white", "nav-short"]);
+    editClasses(navLogo, ["opacity-transition", "invisible"]);
   } else {
-    // navList.classList.remove("nav-white-transition");
     navList.classList.remove("nav-white");
   }
-  // if (scrolled >= vh) {
-  //   console.log("aca");
-  // }
 }
 
 //JQuery for scroll animation (footer and portfolio)
@@ -281,13 +234,28 @@ window.addEventListener("scroll", showWeddingImg);
 let galleryImages = document.querySelectorAll(".img-container img");
 let textButtons = document.querySelectorAll(".img-container .text");
 
-console.log(textButtons);
+// console.log(textButtons);
 
 textButtons.forEach(function(button) {
   button.addEventListener("click", function() {
     alert("me clickearon");
   });
 });
+
+// Auxiliar functions
+
+// add on remove style classes
+function editClasses(element, classes, add = true) {
+  if (add) {
+    classes.forEach(cl => element.classList.add(`${cl}`));
+  } else {
+    classes.forEach(cl => element.classList.remove(`${cl}`));
+  }
+}
+
+//----------------------/////////----------------------//
+// ---------------------// END //---------------------//
+//---------------------/////////---------------------//
 
 // ---------------------
 // gsap!
